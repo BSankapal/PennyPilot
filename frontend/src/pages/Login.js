29c7287bdf4b92d77480819882137569c47cbbe9
BSankapal <Balaji.Sankapal@iiitb.ac.in>
2023-12-01 11:19:40 +0530

    Second Commit

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,18 +4,18 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import "../styles/Loginpage.css";
+
 const Login = () => {
-  const img =
-    "https://images.unsplash.com/photo-1593538312308-d4c29d8dc7f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  //from submit
+
+  // Form submit handler
   const submitHandler = async (values) => {
     try {
       setLoading(true);
       const { data } = await axios.post("/api/v1/users/login", values);
       setLoading(false);
-      message.success("login success");
+      message.success("Login success");
       localStorage.setItem(
         "user",
         JSON.stringify({ ...data.user, password: "" })
@@ -23,46 +23,38 @@ const Login = () => {
       navigate("/");
     } catch (error) {
       setLoading(false);
-      message.error("something went wrong");
+      message.error("Something went wrong");
     }
   };
 
-  //prevent for login user
+  // Prevent login for already logged in users
   useEffect(() => {
     if (localStorage.getItem("user")) {
       navigate("/");
     }
   }, [navigate]);
-  return (
-    <>
-      <div className="login-page ">
-        {loading && <Spinner />}
-        <div className="row container">
-          <h1>PennyPilot</h1>
-          <div className="col-md-6">
-            <img src={img} alt="login-img" width={"100%"} height="100%" />
-          </div>
-          <div className="col-md-4 login-form">
-            <Form layout="vertical" onFinish={submitHandler}>
-              <h1>Login Form</h1>
 
-              <Form.Item label="Email" name="email">
-                <Input type="email" required />
-              </Form.Item>
-              <Form.Item label="Password" name="password">
-                <Input type="password" required />
-              </Form.Item>
-              <div className="d-flex justify-content-between">
-                <Link to="/register">
-                  Not a user ? Click Here to regsiter !
-                </Link>
-                <button className="btn">Login</button>
-              </div>
-            </Form>
+  return (
+    <div className="login-page">
+      {loading && <Spinner />}
+      <div className="login-form">
+        <Form layout="vertical" onFinish={submitHandler}>
+          <h1>Login Form</h1>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <div className="d-flex justify-content-between">
+            <Link to="/register">
+              Not a user? Click Here to register!
+            </Link>
+            <button className="btn">Login</button>
           </div>
-        </div>
+        </Form>
       </div>
-    </>
+    </div>
   );
 };
 
