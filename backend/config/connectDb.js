0fc5173d9f670235451a097ddd5594d945c3e2da
BSankapal <Balaji.Sankapal@iiitb.ac.in>
2023-12-11 22:44:03 +0530

    updated mongodb in frontend

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -4,8 +4,10 @@ mongoose.set('strictQuery', false);
 const colors = require("colors");
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
+    // await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("mongodb://mongodb:27018/PennyPilot");
+    // await mongoose.connect(process.env.MONGO_URL);
+    // console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`${error}`.bgRed);
   }
