ebc8f88c5c226196224e0c21e1ffbcc2033645af
BSankapal <Balaji.Sankapal@iiitb.ac.in>
2023-12-12 11:24:11 +0530

    updated mongodb in backend

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -5,7 +5,8 @@ const colors = require("colors");
 const connectDb = async () => {
   try {
     // await mongoose.connect(process.env.MONGO_URL);
-    await mongoose.connect("mongodb://127.0.0.1:27017");
+    // await mongoose.connect("mongodb://127.0.0.1:27017");
+    await mongoose.connect("mongodb://127.0.0.1:27017"+'/code',{useNewUrlParser:true,useUnifiedTopology: true});
     // await mongoose.connect(process.env.MONGO_URL);
     // console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
